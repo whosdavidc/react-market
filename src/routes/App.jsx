a91903323b787ad08a992ef823cffee039389758
whosdavidc <davinchocastro@gmail.com>
2021-10-12 09:53:47 -0300

    adding-routes

---
+++ src/routes/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
+import "../styles/Login.scss";
+import "../styles/global.css";
+import RecoveryPassword from '../containers/RecoveryPassword';
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Layout>
+					<Route exact path="/" component={ Home }/>
+					<Route exact path="/login" component={ Login }/>
+					<Route exact path="/recovery-password" component={ RecoveryPassword }/>
+					<Route component={ NotFound } />
+				</Layout>
+			</Switch>
+		</BrowserRouter>
+	);
+}
+
+export default App;
