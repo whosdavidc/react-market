848fe6b9bfb3d0395591dfd55693fb98404adead
whosdavidc <davinchocastro@gmail.com>
2021-10-14 16:23:24 -0300

    main routes and styles

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,6 +9,12 @@ import "../styles/Header.scss"
 import RecoveryPassword from '../containers/RecoveryPassword';
 import NotFound from '../pages/NotFound';
 import Home from "../pages/Home";
+import Checkout from '../pages/Checkout';
+import CreateAccount from '../pages/CreateAccount';
+import MyAccount from '../pages/MyAccount';
+import NewPassword from '../pages/NewPassword';
+import Orders from '../pages/Orders';
+import SendEmail from '../pages/SendEmail';
 
 const App = () => {
 	return (
@@ -18,6 +24,13 @@ const App = () => {
 					<Route exact path="/" component={ Home }/>
 					<Route exact path="/login" component={ Login }/>
 					<Route exact path="/recovery-password" component={ RecoveryPassword }/>
+					<Route exact path="/checkout" component={ Checkout }/>
+					<Route exact path="/create-account" component={ CreateAccount }/>
+					<Route exact path="/my-account" component={ MyAccount }/>
+					<Route exact path="/new-password" component={ NewPassword }/>
+					<Route exact path="/orders" component={ Orders }/>
+					<Route exact path="/send-mail" component={ SendEmail }/>
+
 					<Route component={ NotFound }/>
 				</Switch>
 			</Layout>
