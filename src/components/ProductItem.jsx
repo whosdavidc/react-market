824693bb4bcadd5e0b429ba182048e10960b8c74
whosdavidc <davinchocastro@gmail.com>
2021-10-25 19:19:55 -0300

    carItem-handleClick

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/ProductItem.scss';
 import icon from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = () => {
+	const [cart, setCart] = useState()
+	const handleClick = () => {
+		setCart("Hello there");
+	} 
+
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -11,9 +16,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClick}>
 					<img src={icon} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
